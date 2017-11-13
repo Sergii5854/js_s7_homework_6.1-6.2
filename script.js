2b389b74fd9aee27edc886e04744f46c50907b3b
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-13 14:37:15 +0200

    add closure function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function debounce (callback, time) {
   var timer
 
@@ -17,3 +16,25 @@ document
   .addEventListener('input', debounce(function (event) {
     console.log(event.target.value)
   }, 250), false)
+
+/////
+
+function value() {  
+ function getNumberOfSymbvol() {    
+      return document.getElementById('todoInput').value.length  
+  }
+ var result = getNumberOfSymbvol()
+
+   function apendRes() {
+        document.addEventListener('keypress', function todoList (event) {
+               if (event.keyCode == 13) {
+                //var input = document.getElementById('todoInput').value.length
+                var input = getNumberOfSymbvol()
+                document.getElementById('result').innerHTML = input   
+                document.getElementById('todoInput').value =''
+            }
+        }, false)
+    }
+   apendRes()
+}
+value()
