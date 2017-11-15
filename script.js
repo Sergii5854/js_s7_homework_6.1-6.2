a9090b3d35f9041b54f86bf58fdf055e917c8a30
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-15 14:18:02 +0200

    ad constructor function

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+function footballTeams(teamName, championshipName, placeTeam) {
 
+    var name = teamName
+    var championship = championshipName
+    var place = placeTeam 
+    
+    
+    this.name = function () {
+        return name
+    }
+
+    this.championship = function () {
+        return championship
+    }
+
+    this.place = function () {
+        return place
+    }
+    return function () {
+        console.log('Team name : ' + name)
+        console.log('Name championship : ' + championship)
+        console.log('Place team : ' + place)
+    }
+}
+
+var footballTeams = new footballTeams('Dynamo Kyiv', 'Ukrainian Premier League', '2')
+footballTeams()
+
+//
 function debounce (callback, time) {
   var timer
 
